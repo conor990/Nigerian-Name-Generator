4745272362468008f1eaa33776e4c1bc7cb25373
Conor Odoemene <conor99055@gmail.com>
2021-02-12 16:07:12 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,119 @@
+var igboBoy = ["Nnamdi","Chibueze","Afamefuna","Amaechi","Arinze","Azuka","Amadi","Beluolisa","Chibuike","Chibuzor","Chidera","Chidozie","Chiemeka","Chimaijem","Chimezie","Chinedu","Chineze","Chuks","Chukwudi","Daluolisa","Zimife","Ekwueme","Emenike","Ezeudo","Golibe","Haluchi","Igwebuike","Iheanacho","Ikechukwu","Ikemefuna","Ugochukwu","Ikenna","Jachike","Jachimike","Jamalumchi","Jamuike","Jidenna","Kachi","Kamsi","Kelechi","Kelenna","Ndubuisi","Nkenna","Wachukwu","Obioma","Deziri","Emeka","Otito","Ubah","Zimuzo","Zamekpere","Akaolisa","Nwanze","Anozie","Azubuike","Cheluchi","Chijidem","Chimezie","Chinonso","Ejikeme","Ezeudo","Ewelike","Uchechi","Ifediaso","Ikechukwu","Ikemba","Jayamma","Jideofor","Kambirinachi","Lotonna","Nwachimereze"];
+
+var igboGirl = ["Chioma", "Adaeze","Ulaku","Adaego","Adaora","Amaka","Chiamaka","Chimamanda","Chidera","Chidumebi","Chijoke","Chinyere","Chika","Asoka","Chinelo","Ihuoma","Ifeoma","Chisom","Daluchi","Uche","Ezinne","Ezichi","Ginika","Ifunanya","Ijeoma","Omalicha","Nwanyioma","Jachinma","Kairaluchi","Kairamara","Amarachi","Kosarachi","Munachi","Ndidi","Ngozi","Nkiruka","Nneka","Nnenna","Osinachi","Obianuju","Ozioma","Onyinye","Soluzochi","Zelunjo","Chidinma","Zinachimere","Akubueze","Akwaeke","Urioma","Ijeawurika","Adaku","Adannaya","Azuka","Chetachi","Chiazokam","Chideziri","Zirachi","Zimuzochi","Zelunjo","Yagazie","Uloma","Urenna","Zamekpere","Sofuchi","Osoluchi","Nkolika","Nkemjika","Jeweluchi","Ijeawele","Chizua"];
+
+var yorubaBoy = ["Seun", "Tunde","Abimbola","Adebayo","Abiola","Adekunle","Adeniyi","Babatunde","Bamidele","Bayiloluwato","Boluwatitololae","Durodola","Erioluwape","Fiyinfoluwa","Igbayinniayomi","Iseoluwa","Jesufiolawemi","Mobolarinwa","Olamide","Olamilekan","Oluwabamigbe","Olukayode","Oluwafikayomi","Oluwaseun","Oluwatobi","Sijuwola","Toluwanimi","Temitope","Taiwo","Gbenga","Oluwadamilare","Olufemi","Ogooluwa","Mosadioluwa","Jesuloba","Obafemi","Iteoluwa","Idowu","Ade","Adebayo","Adebowale","Adedeji","Adelomo","Adetokunbo","Afolabi","Alaba","Araoluwakiitan","Akingbagbohun","Adebowale","Damilare","Abidun","Abioye","Adelaja","Babajide","Babawale","Bikoseoluwa","Diekoolaoluwa","Eniobafe","Eyitomilayo","Maseso","Obanlanijesu","Ojumirayo","Semilore","Oyebanji","Sijuade","Oluwatoosaba","Oluwasegun","Oluwarotimi","Dimeji","Eyitayo"];
+
+var yorubaGirl = ["Omolayo", "Funke","Abimbola","Abisola","Adebimpe","Adeola","Ayodele","Ebunoluwa","Eniola","Ewatomi","Eyiwunmi","Erimipe","Folakemi","Fadesewa","Folashade","Gbemisola","Ifeoluwako","Itunuoluwa","Iyanuayo","Kikelomo","Mobalaji","Morenike","Morotolaoluwa","odunayo","Ololade","Yewande","Oluwafunke","Funmilayo","Omotade","Omolara","Tiaraoluwa","Oluwatosin","Omobobola","Oreoluwa","Oyenike","Pipeloluwa","Simisola","Temilade","Sibesibeoluwa","Titilayo","Toluwalashe","Wuraola","Yejide","Yeyetunde","Yosolaoluwa","Temilade","Tanitoluwa","Oyinlola","Orode","Opeyimika","Abeni","Abifoluwa","Abioye","Adepeju","Digola","Ereadura","Erimipe","Fadekemi","Idunnuola","Irekanmi","Irekitan","Iyinoluwa","Kikeope","Mojisola","Monifa","Morayo","Titilope","Oreofe","Opeyemi","Omolola"];
+
+var edoBoy =["Abaku","Abeh","Abudu","Adesusu","Adodo","Adoni","Adun","Airewele","Bagwa","Bamiyi","Bazuaye","Bosa","Dauda","Dizola","Dokpesi","Degbueyi","Doni","Ebengho","Ebikade","Ebuehi","Ediku","Idumwonyi","Idusuyi","Ifaluyi","Igbinovia","Igen","Ighalo","Igunbor","Jatto","Joromi","Kadiri","Kokobi","Legema","Noredia","Obamina","Obaseki","Obayagbona","Ogbe","Ogbomo","Omokaro","Omoruyi","Onaghise","Regie","Salami","Salawe","Suberu","Uwagboe","Woghiren","Zuki","Zomi","Abebe","Aburay","Adesagbon","Abusomwan","Aburekhian","Agboghidi","Agbonmwanre","Agbonzee","Bamawo","Bamiyi","Diagbonya","Ebiowe","Edigin","Edebiri","Falodun","Fosa","Guobadia","Idah","Igbinadolor","Nosa"];
+
+var edoGirl = ["Abies","Ahunwan","Abiemwense","Abieyuwa","Adesuwa","Aghabiomo","Aitemini","Ebehiweokhin","Ebiuwairo","Edewi","Edoisiuwa","Eghe","Esohe","Fuje","Ibieuwairo","Iden","Idia","Ifueko","Ighiwiyisi","Ikuero","Imatiti","Imiamiyemwen","Irowiri","Iyengumwena","Izehi","Nanare","Noghayin","Oghorhose","Ohomoime","Okanokan","Omoehiomwan","Omokhose","Morose","Omozusi","Osaigioduwa","Oseluese","Ulamen","Uvbi","Uwafiokun","Yoname","Zena","Zuwa","Zogie","Yonawan","Tiyosayi","Teghese","Otoibhi","Esomo","Izegbuwa","Eseosa","Zona","Zogie","Yalekhue","Uvbinogbe","Ugheghe","Sogie","Shukpeka","Otibhor","Osoyenmwen","Osikhuemeh","Oseki","Osazee","Ononofuje","Onah","Omozuwa","Ozusi","Noyoze","Noyin","Notayo","Kenekene"];
+
+var hfBoy = ["Usman","Abdullah","Balarabe","Danjuma","Danladi","Danlami","Gaddo","Gambo","Gowon","Rashidi","Tanimu","Yohance","Daran","Dare","Daren","Hasan","Rago","Fodio","Muhammadu","Abu Bakr","Modibbo","Ahmadu","Shehu","Alhaji","Gidado","Umaru","Atiku","Sa'adu","Rabiu","Ibrahim","Ado","Sanusi","Ruqayyah","Bala","Mansur","Rilwanu","Jubril","Nuhu","Aliyu","Nasir","Karamokho","Almaami","Yacine","Siradiou","Addi","Demba","Mousa","Jummai","Talatu","Faruq","Sadou","Issa","Bello","Oumarou","Djaili","Mamadou","Hama","Abdel","Hindou","Amad","Habib","Haruna","Masahudu","Umar","Wakaso","Dahiru","Ja'afar","Tijani","Aliyu","Salihu"];
+
+var hfGirl = ["Nana","Amina","A'isha","Aminah","Asma'u","Atikah","Fa'idah","Fa'iqah","Fa'izah","Habibah","Halimah","Hamidah","Iman","Jamilah","Kubra","Latifah","Lubabah","Mansurah","Maimuna","Namilah","Nafisah","Na'imah","Rabi'ah","Rahmah","Rashidah","Safiyah","Sakinah","Salimah","Samirah","Shukriyah","Sumayyah","Zahrah","Zakiyyah","Zaytun","Zubaydah","Halilu","Djenabou","Aissatou","Fatoumata","Binta","Dalanda","Kadiatou","Houssainatou","Hadiza","Yasmine","Samira","Asma","Munirat","Naziru","Malam","Samira","Khady","Hindou","Aissa","Anaca","Assibi","Aunika","Casiah","Dalma","Donae","Durah","Farih","Gambo","Jummai","Nissah","Rinnah","Zaria","Yelwa","Annakya","Atuma"];
+
+function display(){
+
+    var rbBoy = document.getElementById("rbBoy");
+    var rbGirl = document.getElementById("rbGirl");
+    var rbIgbo = document.getElementById("rbIgbo");
+    var rbYoruba = document.getElementById("rbYoruba");
+    var rbEdo = document.getElementById("rbEdo");
+    var rbHF = document.getElementById("rbHF");
+
+    var random = Math.floor(Math.random() * 71); 
+
+    if(rbBoy.checked == true){
+        if(rbIgbo.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = igboBoy[i];
+            }
+        }
+    }
+
+    if(rbGirl.checked == true){
+        if(rbIgbo.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = igboGirl[i];
+            }
+        }
+    }
+
+
+    if(rbBoy.checked == true){
+        if(rbYoruba.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = yorubaBoy[i];
+            }
+        }
+    }
+
+    if(rbGirl.checked == true){
+        if(rbYoruba.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = yorubaGirl[i];
+            }
+        }
+    }
+
+
+    if(rbBoy.checked == true){
+        if(rbEdo.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = edoBoy[i];
+            }
+        }
+    }
+
+    if(rbGirl.checked == true){
+        if(rbEdo.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = edoGirl[i];
+            }
+        }
+    }
+
+
+    if(rbBoy.checked == true){
+        if(rbHF.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = hfBoy[i];
+            }
+        }
+    }
+
+    if(rbGirl.checked == true){
+        if(rbHF.checked == true){
+            for(var i = 0; i < random; i++){
+                document.getElementById("generate").innerHTML = hfGirl[i];
+            }
+        }
+    }
+
+
+
+    if(rbBoy.checked == false){
+        if(rbGirl.checked == false){
+            alert("You must select a gender ");
+        }
+    }
+
+    if(rbIgbo.checked == false){
+        if(rbYoruba.checked == false){
+            if(rbEdo.checked == false){
+                if(rbHF.checked == false){
+                    alert("You must also select an ethnic group");
+                }
+            }
+        }
+    }
+
+ 
+
+
+
+}
+
+
